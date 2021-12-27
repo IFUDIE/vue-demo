d71457f85910ee2ddbfe22195d8f18500f681c11
IFUDIE <2410415376@qq.com>
2021-12-27 22:17:13 +0800

    新增代码

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+import VueRouter from 'vue-router'
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(VueRouter);
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  router,
+  render: h => h(App)
+});
---
+++ src/views/Login.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <el-form ref="form" :model="form" :rules="rules" class="login-box">
+      <el-form-item label="账号" prop="name">
+        <el-input
+          type="text"
+          placeholder="请输入账号"
+          v-model="form.name"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input
+          type="password"
+          placeholder="请输入密码"
+          v-model="form.password"
+        ></el-input>
+      </el-form-item>
+      <el-button type="primary" @click="submitForm('form')">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      form: {
+        name: "",
+        password: "",
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$router.push("/main");
+        } else {
+          this.$message({
+            message: "用户名或密码错误",
+            type: "warning",
+          });
+          return false;
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.login-box {
+  width: 350px;
+  margin: 150px auto;
+  border: 1px solid #d5d9e2;
+  padding: 20px;
+}
+</style>
